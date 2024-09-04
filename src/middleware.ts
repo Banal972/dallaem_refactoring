c6753f768d218dbd0f5d81a2bddd7fbec6c48c95
Kim Ji Yu <spbabo97@naver.com>
2024-09-04 17:11:41 +0900

    fix : middleware 오류 수정

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,42 +1,19 @@
-import { NextResponse } from "next/server"
-import type { NextRequest } from "next/server"
-
 import { auth } from "@/auth"
 
 import ROUTE from "./constants/route"
 
-function startsWith(request: NextRequest, path: string) {
-  return request.nextUrl.pathname.startsWith(path)
-}
-
-function toPrivateRedirect(request: NextRequest, path: string) {
-  return NextResponse.redirect(new URL(`${path}?alert=로그인 후 이용이 가능합니다.`, request.url))
-}
-
-function toPublicRedirect(request: NextRequest, path: string) {
-  return NextResponse.redirect(new URL(`${path}?alert=잘못된 접속 입니다.`, request.url))
-}
-
-export async function middleware(request: NextRequest) {
-  const userToken = await auth()
-
-  const checkUserToken = !!userToken
-
-  if (!checkUserToken) {
-    if (startsWith(request, ROUTE.SAVE_GATHERINGS) || startsWith(request, ROUTE.MY_PAGE)) {
-      return toPrivateRedirect(request, ROUTE.HOME)
-    }
+// eslint-disable-next-line consistent-return
+export default auth((req) => {
+  if (!req.auth && req.nextUrl.pathname === "/mypage") {
+    const newUrl = new URL(ROUTE.HOME, req.nextUrl.origin)
+    return Response.redirect(newUrl)
   }
-
-  if (checkUserToken) {
-    if (startsWith(request, "/auth")) {
-      return toPublicRedirect(request, ROUTE.HOME)
-    }
+  if (!req.auth && req.nextUrl.pathname === "/wishlist") {
+    const newUrl = new URL(ROUTE.HOME, req.nextUrl.origin)
+    return Response.redirect(newUrl)
   }
-
-  return null
-}
+})
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|icon/.*\\.svg|jpg|png).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 }
