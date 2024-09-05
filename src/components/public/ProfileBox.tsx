1c31c4aeadba22784e92fbfc9d26074c36b48e15
JeongHeon <kdrlawjdgjs@gmail.com>
2024-09-05 10:55:07 +0900

    fix: 레이아웃 버그 수정
    - 넓이가 충분함에도 ellipsis처리되는 버그 발생
    - 부모 자식관 width공유 시 에러 발생
    - 관련 css 수정

--- src/components/public/ProfileBox.tsx
+++ src/components/public/ProfileBox.tsx
@@ -9,19 +9,19 @@ const ProfileBox = async () => {
   const userInfo = await getUserInfo()
 
   return (
-    <div className="relative mx-auto h-[180px] min-w-72 w-full flex-none rounded-3xl border-2 border-gray-200 bg-white">
+    <div className="relative mx-auto h-[180px] min-w-60 w-full flex-none rounded-3xl border-2 border-gray-200 bg-white">
       <div className="flex items-center justify-between rounded-t-3xl bg-primary px-6 py-3.5 pt-5">
         <h3 className="z-10 text-lg font-semibold text-gray-900">내 프로필</h3>
         <ProfileEditBtn />
       </div>
       <ProfileBG className="absolute right-5 sm:right-20 top-[19px]" />
-      <div className="flex pl-3">
+      <div className="flex px-3">
         <div className="relative -top-3 size-14 rounded-full shrink-0">
           <Profile profileImg={userInfo.image} state="largeDefault" />
         </div>
-        <div className="px-2 py-3 w-full shrink-1">
+        <div className="px-2 py-3 shrink-1 w-3/4">
           <div className="mb-1 flex items-center gap-3">
-            <h3 className="font-medium text-ellipsis overflow-hidden break-words whitespace-nowrap">{userInfo?.name}</h3>
+            <h3 className="font-medium text-ellipsis overflow-hidden whitespace-nowrap">{userInfo?.name}</h3>
             <LogoutBtn />
           </div>
           <div className="flex gap-2">
@@ -29,9 +29,9 @@ const ProfileBox = async () => {
               <p className="mb-1">company.</p>
               <p>E-mail.</p>
             </div>
-            <div className="text-sm text-gray-800 text-ellipsis overflow-hidden break-words whitespace-nowrap">
-              <p className="mb-1 w-1/2 text-ellipsis overflow-hidden break-words whitespace-nowrap">{userInfo?.companyName}</p>
-              <p className="w-1/2 text-ellipsis overflow-hidden break-words whitespace-nowrap">{userInfo?.email}</p>
+            <div className="text-sm text-gray-800 overflow-hidden w-full">
+              <p className="mb-1 text-ellipsis overflow-hidden w-full whitespace-nowrap">{userInfo?.companyName}</p>
+              <p className="text-ellipsis overflow-hidden w-full whitespace-nowrap">{userInfo?.email}</p>
             </div>
           </div>
         </div>
