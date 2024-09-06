b40b79cefa10a578cd7471ea5b5266268dcefa02
JeongHeon <kdrlawjdgjs@gmail.com>
2024-09-06 13:35:21 +0900

    refactor: 같은 동작하는 컴포넌트 분리 및 통합
    - 페이지 별로 같은 동작하는 테그 발견
    - 통합 및 분리

--- src/app/wishlist/page.tsx
+++ src/app/wishlist/page.tsx
@@ -1,27 +1,11 @@
-import Image from "next/image"
-
 import List from "@/components/pages/wishlist/List"
-import HeadSavedIMG from "@public/img/head_saved.png"
+import ImageHeader from "@/components/public/ImageHeader"
 
 const WishListPage = () => {
   return (
     <main>
       <div className="m-6 flex min-h-screen flex-col rounded-[20px] bg-white px-6 py-14 md:m-12 md:px-16">
-        <div className="flex-none">
-          <div className="flex items-center gap-4 sm:gap-[13px]">
-            <div className="size-[72px] flex-none">
-              <Image width={72} height={72} src={HeadSavedIMG.src} alt="HeadSavedIMG" />
-            </div>
-            <div>
-              <h4 className="text-lg font-semibold leading-8 text-gray-900 sm:text-2xl">
-                찜한 모임
-              </h4>
-              <p className="left-5 mt-2 text-sm font-medium text-gray-700">
-                마감되기 전에 지금 바로 참여해보세요 👀
-              </p>
-            </div>
-          </div>
-        </div>
+        <ImageHeader sort="wish" mainText={mainText} subText={subText} />
         <List />
       </div>
     </main>
@@ -29,3 +13,6 @@ const WishListPage = () => {
 }
 
 export default WishListPage
+
+const mainText = "찜한 모임"
+const subText = "마감되기 전에 지금 바로 참여해보세요 👀"
